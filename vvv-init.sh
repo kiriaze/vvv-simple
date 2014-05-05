fbcc9ec5f969d84a932f75228848accb59795d22
Constantine Kiriaze <ckiriaze@gmail.com>
2014-05-05 12:53:44 -0700

    quick search/replace

--- vvv-init.sh
+++ vvv-init.sh
@@ -1,11 +1,11 @@
-# Init script for VVV Auto Bootstrap Demo 2
+# Init script for VVV Auto Bootstrap Simple
 
-echo "Commencing VVV Demo 2 Setup"
+echo "Commencing VVV Simple Setup"
 
 # Make a database, if we don't already have one
 echo "Creating database (if it's not already there)"
-mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS vvv_demo_2"
-mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON vvv_demo_2.* TO wp@localhost IDENTIFIED BY 'wp';"
+mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS vvv_simple"
+mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON vvv_simple.* TO wp@localhost IDENTIFIED BY 'wp';"
 
 # Run Composer
 composer install --prefer-dist
@@ -14,11 +14,11 @@ composer install --prefer-dist
 if [ ! -f wp/wp-config.php ]
 then
 	echo "Creating wp-config.php and installing WordPress"
-	wp core config --dbname="vvv_demo_2" --dbuser=wp --dbpass=wp --dbhost="localhost"
-	wp core install --url=vvv-demo-2.dev --title="VVV Bootstrap Demo 2" --admin_user=admin --admin_password=password --admin_email=demo@example.com
+	wp core config --dbname="vvv_simple" --dbuser=wp --dbpass=wp --dbhost="localhost"
+	wp core install --url=vvv-simple.dev --title="VVV Bootstrap Simple" --admin_user=admin --admin_password=password --admin_email=demo@example.com
 fi
 
 # The Vagrant site setup script will restart Nginx for us
 
-echo "VVV Demo 2 site now installed";
+echo "VVV Simple site now installed";
 
